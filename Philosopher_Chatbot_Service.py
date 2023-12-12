2eb2ecea85b026e11f12c9ab53ae8762a697909f
annayjs <102045970+annayjs@users.noreply.github.com>
2023-12-13 01:01:27 +0900

    Update Philosopher_Chatbot_Service.py

--- Philosopher_Chatbot_Service.py
+++ Philosopher_Chatbot_Service.py
@@ -139,7 +139,6 @@ if submit_button and user_message:
         위 상담 내용에 대해, 위 구절과 %s의 사상을 바탕으로 %d자 이내로, %s의 말투를 사용해서 마치 %s가 말하듯이 친절하게 상담해줘.
         """%(user_message, chosen_philosopher, input_text.iloc[0], input_text.iloc[1], input_text.iloc[2], 
              chosen_philosopher, max_tokens, chosen_philosopher, chosen_philosopher)
-    print(user_prompt)
     user_prompt_eng=translator.translate_text(user_prompt, target_lang="KO").text
     st.session_state.messages.append({"role": "user", 
                                       "content": user_prompt_eng+'@@@'+user_message})
@@ -169,11 +168,12 @@ for message in st.session_state.messages:
         input_message = message['content'].split('@@@')[1]
         st.write("🙋‍♂나:")
         st.write(input_message)
+        st.write(message['content'])
         st.write("_________________________________________________________________________________________________________")
         # 정규식 패턴: 괄호 사이의 값을 찾음
         pattern = r'\((.*?)\)'    
         # 정규식을 사용하여 괄호 안의 값을 찾음
-        matches = re.findall(pattern, input_message)
+        matches = re.findall(pattern, message['content'].split('@@@')[0])
         st.write("참고 저서: \n", matches)
     elif message["role"] == "assistant":
         gpt_answer = message['content'].split('@@@')[0]
