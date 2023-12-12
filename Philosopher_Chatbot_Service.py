f87c45e8fc06a87b4becea232787770d9f55c444
annayjs <102045970+annayjs@users.noreply.github.com>
2023-12-13 03:21:28 +0900

    Update Philosopher_Chatbot_Service.py

--- Philosopher_Chatbot_Service.py
+++ Philosopher_Chatbot_Service.py
@@ -214,8 +214,8 @@ for message in st.session_state.messages:
             part=part.replace('{', ' ')
             part=part.replace('}', ' ')
             part_ko=translator.translate_text(part, target_lang='KO').text
-            formatted_text = re.sub(r"(?<!^)(\d+\.)", r"\n\1", part_ko)
-            st.write(formatted_text)
+            #formatted_text = re.sub(r"(?<!^)(\d+\.)", r"\n\1", part_ko)
+            st.write(part_ko)
     elif message["role"] == "assistant":
         gpt_answer = message['content'].split('@@@')[0]
         st.write("🧔 %s: "%(message['content'].split('@@@')[1]))
